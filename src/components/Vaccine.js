465e43ce9467e21d4a2f4af62f9a7fd932d2fe22
MEnghi44 <69116659+MEnghi44@users.noreply.github.com>
2021-08-17 15:13:21 +0700

    u

--- src/components/Vaccine.js
+++ src/components/Vaccine.js
@@ -3,7 +3,19 @@ import './Vaccine.css';
 import VacImage from './VacImage';
 
 class Vaccine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shot: '10000000',
+      have: true,
+    };
+  }
+
+
   render() {
+    if (!this.state.have) {
+      return <div>Not found</div>;
+    }
     return (
       <div className="vaccine__list">
         <VacImage path="/img/1.jpg" />
@@ -11,10 +23,11 @@ class Vaccine extends React.Component {
         <div className="vaccine__item">{this.props.nameInter}</div>
         <div className="vaccine__item">{this.props.produce}</div>
         <div className="vaccine__item">{this.props.technology}</div>
+        <div>{this.state.shot}</div>
+        <div>{`${this.state.have}`}</div>
       </div>
     );
   }
 }
 
-// export const Vac = Vaccine;
 export default Vaccine;
--- src/index.js
+++ src/index.js
@@ -2,35 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Vaccine from './components/Vaccine';
 
-const App = () => {
-  return (
-    <div className='vaccine'>
-      <Vaccine
-        nameCompony="Astrazeneca"
-        nameInter="AZD1222"
-        produce="England"
-        technology="ไวรัส Adonovirus พาหะ"
-      />
-      <Vaccine
-        nameCompony="Phizer"
-        nameInter="BNT162"
-        produce="USA"
-        technology="mRNA"
-      />
-      <Vaccine
-        nameCompony="moderna"
-        nameInter="mRNA-1273"
-        produce="USA"
-        technology="mRNA"
-      />
-      <Vaccine
-        nameCompony="sinovac"
-        nameInter="Coronavac"
-        produce="China"
-        technology="วัคซันเชื้อตาย"
-      />
+class App extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+        items: [
+          {
+            nameCompony:"Astrazeneca",
+            nameInter:"AZD1222",
+            produce:"England",
+            technology:"ไวรัส Adonovirus พาหะ",
+          },
+          {
+            nameCompony:"Phizer",
+            nameInter:"BNT162",
+            produce:"USA",
+            technology:"mRNA",
+          },
+        ]
+      }
+    }
+  
+  render (){
+    
+    const itemsRendered = this.state.items.map((item) => {
+      return(
+        <Vaccine
+        nameCompony={item.nameCompony}
+        nameInter={item.nameInter}
+        produce={item.produce}
+        technology={item.technology}
+        />
+      );
+    });
+
+    return <div className="vaccine">
+    {itemsRendered}
     </div>
-  );
-};
+    
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
